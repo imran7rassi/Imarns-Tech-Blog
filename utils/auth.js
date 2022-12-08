d1ff291c824dc04e5e09b0603c4a1b5d3487cd8f
imran-rassi <imran.rassi.2020@gmail.com>
2022-12-08 18:33:59 -0500

    updated some changes

--- utils/auth.js
+++ utils/auth.js
@@ -1,8 +1,13 @@
-// middleware to verify user loggied in before restricted route access given
+
+// middleware to verify user 
+//loggied in before restricted
+// route access given
 const withAuth = (req, res, next) => {
     if (!req.session.userId) {
       res.redirect("/login");
     } else {
+
+      // we use here the next function //
       next();
     }
   };
