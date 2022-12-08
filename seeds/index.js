30d5c0c9ea9491395c3b8705facea03658676ab2
imran-rassi <imran.rassi.2020@gmail.com>
2022-12-08 16:34:04 -0500

    updated some files

---
+++ seeds/index.js
@@ -0,0 +1,17 @@
+
+
+const sequelize = require('../config/config');
+const seedUser = require('./userData');
+const seedPost = require('./postData');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+
+  await seedUser();
+
+  await seedPost();
+
+  process.exit(0);
+};
+
+seedAll();
