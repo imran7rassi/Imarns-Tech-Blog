eb36eed070cbc0d410a28ad589132eea19efafc7
imran-rassi <imran.rassi.2020@gmail.com>
2022-12-08 20:35:35 -0500

    deploy heroku

--- models/post.js
+++ models/post.js
@@ -1,8 +1,13 @@
+
+// connecting the sequelize //
 const { Sequelize, Model, DataTypes } = require('sequelize');
+// connecting the connection.js //
 const sequelize = require('../config/connection.js');
 
+// class for post //
 class Post extends Model {}
 
+// init the post //
 Post.init(
   {
     title: DataTypes.STRING,
