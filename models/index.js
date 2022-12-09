0d39f06936da6a7e04b2173c7676ace0b4225863
imran-rassi <imran.rassi.2020@gmail.com>
2022-12-08 22:27:38 -0500

    updates

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@
 // connecting the user //
 const User = require('./User');
 // connecting the post.js //
-const Post = require('./poosti');
+const Post = require('./post');
 // connecting the coment //
 const Comment = require('./Comment');
 
